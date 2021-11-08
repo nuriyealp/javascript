2fb503311b9816c0f4b61d9eb77d4effe5b798ac
Nur ALP <88150284+nuriyealp@users.noreply.github.com>
2021-11-08 07:12:11 -0800

    declaring and assigning variables

---
+++ declare-assign.js
@@ -0,0 +1,15 @@
+// var, let, const
+
+// declaring variables
+var firstName;
+let lastName;
+const location = "USA"
+
+// assigning values
+firstName = "Nuriye";
+lastName = "Alp";
+
+
+//declare and assign
+let favoriteColor = "blue";
+const myAge = 33;
