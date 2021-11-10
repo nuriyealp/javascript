479a2ad471d37d0fefb66d0cbeabe90398a50d80
Nur ALP <88150284+nuriyealp@users.noreply.github.com>
2021-11-09 21:13:51 -0800

    mathematic operations

---
+++ operations.js
@@ -0,0 +1,27 @@
+// add, subtract, multiply, divide//
+const number = 120;
+const count = 10;
+
+let add = number + count;
+console.log(add);
+
+let subtract = number - count;
+console.log(subtract);
+
+let multiply = number * count;
+console.log(multiply);
+
+let divide = number / count;
+console.log(divide);
+
+//increment bir artirir
+divide++;
+console.log(divide);
+//decrement bir azaltir
+multiply--;
+console.log(multiply);
+
+
+// finding the remainder using modulo ->%
+const tree = number % count;
+console.log(tree);
