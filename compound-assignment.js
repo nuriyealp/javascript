0d2dd2fb8f67b4774debc70a90efcaa7ea5c85d1
Nur ALP <88150284+nuriyealp@users.noreply.github.com>
2021-11-10 22:41:56 -0800

    compound-assignment

---
+++ compound-assignment.js
@@ -0,0 +1,25 @@
+// augmented addition
+let flower = 1;
+console.log(flower);
+//flower = flower + 3;
+flower += 3;
+console.log(flower);
+
+// augmented subtraction
+let apple = 3;
+console.log(apple);
+apple -= 2;
+console.log(apple);
+
+//augmented multiplication
+let orange = 5;
+console.log(orange);
+orange *= 6;
+console.log(orange);
+
+
+//division
+let chocolate = 8;
+console.log(chocolate);
+chocolate /= 2;
+console.log(chocolate);
