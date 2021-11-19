0a8074b8b135a689cb6f6412fb0082a310038387
Nur ALP <88150284+nuriyealp@users.noreply.github.com>
2021-11-18 21:37:37 -0800

    strings

---
+++ strings.js
@@ -0,0 +1,7 @@
+//escaping strings
+const difficult = "I am a \"double quoted\" string inside \"double quotes\".";
+console.log(difficult);
+
+//concatenating strings
+const easy = "l am so busy. " + "l am so exhausted";
+console.log(easy);
