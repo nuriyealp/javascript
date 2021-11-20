2cc2408234d3b018a9036794b0834c62a15b056e
Nur ALP <88150284+nuriyealp@users.noreply.github.com>
2021-11-20 09:49:40 -0800

    strings

--- strings.js
+++ strings.js
@@ -5,3 +5,25 @@ console.log(difficult);
 //concatenating strings
 const easy = "l am so busy. " + "l am so exhausted";
 console.log(easy);
+
+
+
+let friday = "it is a fun day.";
+friday += "it is a nice day.";
+console.log(friday);
+
+
+const floWer = "awesome!";
+let myStr = "beautifulDaughter";
+myStr += floWer;
+
+console.log("learning".length)
+const tree = "green";
+console.log(tree.length)
+
+
+let lastName = "alp";
+const said = lastName[0];
+console.log(said);
+const tell = lastName[2];
+console.log(tell);
