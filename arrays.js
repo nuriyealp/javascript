1c9f97cd3ae286497b6269eb2815a4a8d1891634
Nur ALP <88150284+nuriyealp@users.noreply.github.com>
2021-11-21 07:52:29 -0800

    arrays

---
+++ arrays.js
@@ -0,0 +1,17 @@
+let salad = ["onion", " cucumber", "tomatoes"];
+
+console.log(salad);
+console.log(salad.length);
+console.log(salad[0]);
+
+let soup = [["chicken"]];
+console.log(soup);
+
+salad.push(2);
+console.log(salad);
+salad.pop();
+console.log(salad);
+salad.shift();
+console.log(salad);
+salad.unshift("boil");
+console.log(salad);
