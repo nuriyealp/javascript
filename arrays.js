5edab68fb82f43f76eef406911634a48d7582710
Nur ALP <88150284+nuriyealp@users.noreply.github.com>
2021-11-22 21:45:05 -0800

    array

--- arrays.js
+++ arrays.js
@@ -1,17 +1,24 @@
 let salad = ["onion", " cucumber", "tomatoes"];
 
-console.log(salad);
-console.log(salad.length);
-console.log(salad[0]);
+// console.log(salad);
+// console.log(salad.length);
+// console.log(salad[0]);
 
 let soup = [["chicken"]];
-console.log(soup);
+// console.log(soup);
 
 salad.push(2);
-console.log(salad);
+// console.log(salad);
 salad.pop();
-console.log(salad);
+// console.log(salad);
 salad.shift();
-console.log(salad);
+// console.log(salad);
 salad.unshift("boil");
-console.log(salad);
+// console.log(salad);
+
+
+// add 5 arrays into an array -> shopping list
+const myList = [];
+// ["apple", 3]
+myList.push(["apple", 3], ["orange", 2], ["water", 1], ["flower", 0], ["tea", 5]);
+console.log(myList);
