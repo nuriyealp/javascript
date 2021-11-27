c4a46f759012c3d69f6dfdcf6cc4b7cf8ec7105e
Nur ALP <88150284+nuriyealp@users.noreply.github.com>
2021-11-27 07:53:14 -0800

    functions

---
+++ functions.js
@@ -0,0 +1,16 @@
+// define the function
+function monkey() {
+  console.log("cute");
+  console.log("flower");
+}
+//call the function
+monkey();
+
+
+let paper = "soft";
+
+// define a function with an argument
+function book(pen) {
+  console.log(paper + " " + pen);
+}
+book("blue");
