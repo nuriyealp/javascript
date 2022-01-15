ce9a81d85028b37ab9e5f69e21927ddac1903652
Nur ALP <88150284+nuriyealp@users.noreply.github.com>
2022-01-15 12:46:26 -0800

    objects

---
+++ objects.js
@@ -0,0 +1,22 @@
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+const hatValue = testObj.hat
+const shirtValue = testObj.shirt
+const shoesValues = testObj.shoes
+
+
+
+const examObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+
+};
+
+const entreeValue = examObj["an entree"]
+const waterValue = examObj["my side"]
+const milkValue = examObj["the drink"]
+
